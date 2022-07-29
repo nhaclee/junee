0dda927b9c508171870434ee00ccecfba0203bd9
nhaclee <108122540+nhaclee@users.noreply.github.com>
2022-07-29 16:33:27 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+var typed = new Typed('.typing-1', {
+    strings: ['THE KING', 'THE SLAYER', 'THE DOOM ETERNAL !!!',],
+    smartBackspace: true,
+    typeSpeed: 30,
+    backSpeed: 50,
+    loop: true,
+    loopCount: Infinity // Default value
+});
